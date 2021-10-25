46530119fe89e5acd7e655a8ce160b646339ac29
Walter Otsyula <wotsyula@gmail.com>
2021-10-25 09:53:28 +0300

    Add mocha chai sinon (#10)
    * install chai, mocha, sinon
    
    * add `.mocharc.js`
    
    * add mocha `beforeEach` and `afterEach` hooks
    
    * fix whitespace at end of file
    
    * add newline at end of files

---
+++ config/setup-mocha.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Set up Chai ----------------------------------
+
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const chaiHTTP = require('chai-http');
+
+chai.use(sinonChai);
+chai.use(chaiHTTP);
+
+// Set up Mocha ----------------------------------
+
+const sleep = (milliseconds) => {
+  if (
+    typeof milliseconds !== 'number' ||
+    milliseconds < 0 ||
+    milliseconds > Number.MAX_SAFE_INTEGER
+  ) {
+    throw new TypeError(`milliseconds must be a number 0 - ${Number.MAX_SAFE_INTEGER}`);
+  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, milliseconds);
+  })
+}
+
+const mochaHooks = {
+  beforeEach() {
+    this.sleep = sleep;
+  },
+
+  afterEach() {
+    delete this.sleep;
+  }
+};
+  
+module.exports = { mochaHooks };
