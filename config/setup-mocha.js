29f34815e44406982dba1b3821caa9705f019e89
Walter Otsyula <wotsyula@ext.copper.com>
2021-10-25 09:30:12 +0300

    add mocha `beforeEach` and `afterEach` hooks

--- config/setup-mocha.js
+++ config/setup-mocha.js
@@ -8,3 +8,32 @@ const chaiHTTP = require('chai-http');
 
 chai.use(sinonChai);
 chai.use(chaiHTTP);
+
+// Set up Mocha ----------------------------------
+
+const sleep = (milliseconds) => {
+  if (
+    typeof milliseconds !== 'number' ||
+    milliseconds < 0 ||
+    milliseconds > Number.MAX_SAFE_INTEGER
+  ) {
+    throw new TypeError(`milliseconds must be a number 0 - ${Number.MAX_SAFE_INTEGER}`);
+  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, milliseconds);
+  })
+}
+
+const mochaHooks = {
+  beforeEach() {
+    this.sleep = sleep;
+  },
+
+  afterEach() {
+    delete this.sleep;
+  }
+};
+  
+module.exports = { mochaHooks };
+  
