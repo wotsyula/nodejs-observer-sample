abb8d83347e869b23f3832ca0af8f5551dcb3fc3
Walter Otsyula <wotsyula@gmail.com>
2021-11-05 13:53:43 +0300

    Create server configuration

---
+++ config/koa.mjs
@@ -0,0 +1,9 @@
+/**
+ * Creates a configuration for `koa-js` server.
+ *
+ * @returns {any} Object to pass to `new require('koa')(config)`
+ */
+export default async function createKoaConfig () {
+  const config = {};
+  return config;
+};
